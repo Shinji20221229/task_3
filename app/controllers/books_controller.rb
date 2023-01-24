7c23d2c5d66f81b8040dfc6f8d4d41ad79997443
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-01-24 11:12:48 +0000

    commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,8 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -18,18 +19,23 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @books = Book.all
-    @user = current_user
+    @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to '/book'
+    redirect_to '/books'
   end
 
    private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @user = User.new
+    @users = User.all
+    @user = current_user
   end
 
   def edit
